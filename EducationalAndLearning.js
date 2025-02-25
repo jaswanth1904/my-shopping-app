8c12640b6e02a1ccc95d9b9d97f1cf1e85287cd7
Jaswanth Ampabathuni <aj19.jaswanth@gmail.com>
2025-02-25 17:41:34 +0530

    Add files via upload

---
+++ EducationalAndLearning.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EducationalAndLearning() {
+  return (
+    <div className="page">
+      <h1>Educational and Learning</h1>
+      <p>Discover educational products.</p>
+    </div>
+  );
+}
+
+export default EducationalAndLearning;
---
+++ HomeAppliances.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomeAppliances() {
+  return (
+    <div className="page">
+      <h1>Home Appliances</h1>
+      <p>Shop for home appliances.</p>
+    </div>
+  );
+}
+
+export default HomeAppliances;
